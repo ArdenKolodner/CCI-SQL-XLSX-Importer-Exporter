7c1bf35f3a72ffe9d31535cf954049d9d1407acf
Arden Kolodner <akolodner25@amherst.edu>
2024-08-12 14:05:34 -0400

    Tabs not spaces

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -1,24 +1,24 @@
 BEGIN TRANSACTION;
 CREATE TABLE "People" (
-	id INTEGER NOT NULL, 
-	"Name" VARCHAR(255), 
-	"Nickname" VARCHAR(255), 
-	PRIMARY KEY (id)
+	id INTEGER NOT NULL,
+	"Name" VARCHAR(255),
+	"Nickname" VARCHAR(255),
+  PRIMARY KEY (id)
 );
-INSERT INTO "People" VALUES(1,'Nicholas','Nick');
-INSERT INTO "People" VALUES(2,'John','Johnny');
-INSERT INTO "People" VALUES(3,'Jane','Jenny');
-INSERT INTO "People" VALUES(4,'Wenceslas','');
+INSERT INTO "People" VALUES('1','Nicholas','Nick');
+INSERT INTO "People" VALUES('2','John','Johnny');
+INSERT INTO "People" VALUES('3','Jane','Jenny');
+INSERT INTO "People" VALUES('4','Wenceslas','');
 CREATE TABLE "Rocks" (
-	id INTEGER NOT NULL, 
-	"Type" VARCHAR(255), 
-	"Origin" VARCHAR(255), 
-	PRIMARY KEY (id)
+	id INTEGER NOT NULL,
+	"Type" VARCHAR(255),
+	"Origin" VARCHAR(255),
+  PRIMARY KEY (id)
 );
-INSERT INTO "Rocks" VALUES(1,'Limestone','a mysterious cave');
-INSERT INTO "Rocks" VALUES(2,'Granite','a riverbed, long since dried up');
-INSERT INTO "Rocks" VALUES(3,'Obsidian','found under my pillow after a particulaly wild night');
-INSERT INTO "Rocks" VALUES(4,'Gneiss','saw it by the side of the road and thought it looked gneiss');
-INSERT INTO "Rocks" VALUES(5,'Chunk of Plastic','');
-INSERT INTO "Rocks" VALUES(6,'Sandstone','a pile of sand had an unfortunate run-in with Medusa');
+INSERT INTO "Rocks" VALUES('1','Limestone','a mysterious cave');
+INSERT INTO "Rocks" VALUES('2','Granite','a riverbed, long since dried up');
+INSERT INTO "Rocks" VALUES('3','Obsidian','found under my pillow after a particulaly wild night');
+INSERT INTO "Rocks" VALUES('4','Gneiss','saw it by the side of the road and thought it looked gneiss');
+INSERT INTO "Rocks" VALUES('5','Chunk of Plastic','');
+INSERT INTO "Rocks" VALUES('6','Sandstone','a pile of sand had an unfortunate run-in with Medusa');
 COMMIT;
